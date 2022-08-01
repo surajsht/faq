b5f76a1f869ac0b2d6525251585ab121bc750005
suraj <surajsht2055@gmail.com>
2022-08-01 23:44:51 +0545

    refactored code not working properly, returned to older code

--- main.js
+++ main.js
@@ -1,81 +1,81 @@
 let show = document.querySelectorAll(".show")
 let hide = document.querySelectorAll(".hide")
 
-// show.forEach(function (show) {
-//   show.addEventListener("click", function (e) {
-//     if (e.target.classList.contains("fa-solid")) {
-//       let parentElement = e.target.parentElement.parentElement
-//       let nextSibling = parentElement.nextElementSibling
-
-//       let siblingContentHeight = nextSibling
-//         .querySelector("p")
-//         .getBoundingClientRect().height
-
-//       nextSibling.style.height = siblingContentHeight + "px"
-
-//       show.style.display = "none"
-
-//       let iconHide = parentElement.querySelector(".hide")
-//       iconHide.style.display = "block"
-//     } else {
-//       return
-//     }
-//   })
-// })
-
-// hide.forEach(function (hide) {
-//   hide.addEventListener("click", function (e) {
-//     if (e.target.classList.contains("fa-solid")) {
-//       let parentElement = e.target.parentElement.parentElement
-//       let nextSibling = parentElement.nextElementSibling
-
-//       nextSibling.style.height = 0 + "px"
-
-//       hide.style.display = "none"
-
-//       let iconShow = parentElement.querySelector(".show")
-
-//       iconShow.style.display = "block"
-//     }
-//   })
-// })
-
-// refactored code 
-
 show.forEach(function (show) {
   show.addEventListener("click", function (e) {
-    showHide(e, "hide")
+    if (e.target.classList.contains("fa-solid")) {
+      let parentElement = e.target.parentElement.parentElement
+      let nextSibling = parentElement.nextElementSibling
+
+      let siblingContentHeight = nextSibling
+        .querySelector("p")
+        .getBoundingClientRect().height
+
+      nextSibling.style.height = siblingContentHeight + "px"
+
+      show.style.display = "none"
+
+      let iconHide = parentElement.querySelector(".hide")
+      iconHide.style.display = "block"
+    } else {
+      return
+    }
   })
 })
 
 hide.forEach(function (hide) {
   hide.addEventListener("click", function (e) {
-    showHide(e, "show")
+    if (e.target.classList.contains("fa-solid")) {
+      let parentElement = e.target.parentElement.parentElement
+      let nextSibling = parentElement.nextElementSibling
+
+      nextSibling.style.height = 0 + "px"
+
+      hide.style.display = "none"
+
+      let iconShow = parentElement.querySelector(".show")
+
+      iconShow.style.display = "block"
+    }
   })
 })
 
-function showHide(e, ele) {
-  if (!e.target.classList.contains("fa-solid")) return
+// refactored code 
 
-  let parentElement = e.target.closest(".faq-header")
-  let nextSibling = parentElement.nextElementSibling
-  let nextSiblingContentHeight = nextSibling
-    .querySelector("p")
-    .getBoundingClientRect().height
+// show.forEach(function (show) {
+//   show.addEventListener("click", function (e) {
+//     showHide(e, "hide")
+//   })
+// })
 
-  if (nextSibling.style.height == 0) {
-    nextSibling.style.height = nextSiblingContentHeight + "px"
-  } else {
-    nextSibling.style.height = 0
-  }
+// hide.forEach(function (hide) {
+//   hide.addEventListener("click", function (e) {
+//     showHide(e, "show")
+//   })
+// })
 
-  e.target.parentElement.style.display = "none"
+// function showHide(e, ele) {
+//   if (!e.target.classList.contains("fa-solid")) return
 
-  let childrens = Array.from(parentElement.children)
+//   let parentElement = e.target.closest(".faq-header")
+//   let nextSibling = parentElement.nextElementSibling
+//   let nextSiblingContentHeight = nextSibling
+//     .querySelector("p")
+//     .getBoundingClientRect().height
 
-  childrens.forEach(function (children) {
-    if (children.classList.contains(ele)) {
-      children.style.display = "block"
-    }
-  })
-}
+//   if (nextSibling.style.height == 0) {
+//     nextSibling.style.height = nextSiblingContentHeight + "px"
+//   } else {
+//     nextSibling.style.height = 0
+//   }
+
+//   e.target.parentElement.style.display = "none"
+
+//   let childrens = Array.from(parentElement.children)
+
+//   childrens.forEach(function (children) {
+//     if (children.classList.contains(ele)) {
+//       children.style.display = "block"
+//     }
+//   })
+// }
